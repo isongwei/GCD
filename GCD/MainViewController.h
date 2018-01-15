64cc89c2ae25b714bd61ddbe3d4843ade8a58a34
张松伟 <zhangsongwei@192.168.0.166>
2018-01-15 23:17:43 +0800

    18.01.15

---
+++ GCD/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  GCD
+//
+//  Created by 张松伟 on 2018/1/4.
+//  Copyright © 2018年 iSong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ GCD/MainViewController.m
@@ -0,0 +1,27 @@
+
+//
+//  MainViewController.m
+//  GCD
+//
+//  Created by 张松伟 on 2018/1/4.
+//  Copyright © 2018年 iSong. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+
+@end
